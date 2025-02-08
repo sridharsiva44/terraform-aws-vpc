27a4a67c7ae9ab06f2006343285952e31788ebd4
sridharsiva44 <r.s.sridharvenkat@gmail.com>
2025-02-08 19:36:57 +0530

    Add Public and Private Subnets - Added public subnets across multiple Availability zone - Added private subnets across multiple Availability zone - Updated variables.tf to include subnet CIDRs & Availability zone

--- variables.tf
+++ variables.tf
@@ -5,3 +5,15 @@ variable "region" {
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
